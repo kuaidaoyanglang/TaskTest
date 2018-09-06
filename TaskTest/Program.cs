2b7ee8081d0d54b79f2cc685f1a92c0dbbfdc762
kuaidaoyanglang <1042744766@qq.com>
2018-09-07 01:12:51 +0800

    first commit

---
+++ TaskTest/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var worker = new Worker();
+            worker.ParallelInit();
+            var t = worker.Init();
+            t.Wait();
+            Console.ReadKey();
+        }
+    }
+
+    public class Worker
+    {
+        public async Task<bool> Init()
+        {
+            var series = Enumerable.Range(1, 5).ToList();
+            foreach (var i in series)
+            {
+                Console.WriteLine("Starting Process {0}", i);
+                var result = await DoWorkAsync(i);
+                if (result)
+                {
+                    Console.WriteLine("Ending Process {0}", i);
+                }
+            }
+
+            return true;
+        }
+
+        public async Task<bool> DoWorkAsync(int i)
+        {
+            Console.WriteLine("working..{0}", i);
+            await Task.Delay(1000);
+            return true;
+        }
+
+        public bool ParallelInit()
+        {
+            var series = Enumerable.Range(1, 5).ToList();
+            Parallel.ForEach(series, i =>
+            {
+                Console.WriteLine("Starting Process {0}", i);
+                DoWorkAsync(i);
+                Console.WriteLine("Ending Process {0}", i);
+            });
+            return true;
+        }
+    }
+}
---
+++ TaskTestForCycle/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTestForCycle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var worker = new Worker();
+            worker.ParallelInit();
+            var t = worker.Init();
+            t.Wait();
+            Console.ReadKey();
+        }
+    }
+
+    public class Worker
+    {
+        public async Task<bool> Init()
+        {
+            var series = Enumerable.Range(1, 5).ToList();
+            var tasks = new List<Task<Tuple<int, bool>>>();
+            foreach (var i in series)
+            {
+                Console.WriteLine("Starting Process {0}", i);
+                tasks.Add(DoWorkAsync(i));
+            }
+            foreach (var task in await Task.WhenAll(tasks))
+            {
+                if (task.Item2)
+                {
+                    Console.WriteLine("Ending Process {0}", task.Item1);
+                }
+            }
+            return true;
+        }
+
+        public async Task<Tuple<int, bool>> DoWorkAsync(int i)
+        {
+            Console.WriteLine("working..{0}", i);
+            await Task.Delay(1000);
+            return Tuple.Create(i, true);
+        }
+
+        public bool ParallelInit()
+        {
+            var series = Enumerable.Range(1, 5).ToList();
+            Parallel.ForEach(series, i =>
+            {
+                Console.WriteLine("Starting Process {0}", i);
+                DoWorkAsync(i);
+                Console.WriteLine("Ending Process {0}", i);
+            });
+            return true;
+        }
+    }
+}
